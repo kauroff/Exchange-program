a39dabdffae2858dc26c71301eb58fde2f83f069
Ivan Kaurov <crazyanimalcz@gamil.com>
2023-08-17 18:18:25 +0300

    Telegram bot

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 import json
 import os
 from datetime import datetime
+import telebot
+from telebot import types
 
 import requests
 
+bot = telebot.TeleBot('TELEGRAM_TOKEN')
 API_KEY = os.getenv('EXCHANGE_RATE_API_KEY')
 CURRENCY_RATES_FILE = 'currency_rates.json'
 
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text:
+        bot.send_message(message.from_user.id, "Привет!\nЯ - бот, который подсказывает курс доллара/евро в данный момент или в указанную дату")
+
+bot.polling(none_stop=True, interval=0)
+
+
+def get_age(message):
+      keyboard = types.InlineKeyboardMarkup() #наша клавиатура
+      key_currency = types.InlineKeyboardButton(text='Узнать курс', callback_data='currency') #кнопка «Да»
+      keyboard.add(key_currency) #добавляем кнопку в клавиатуру
+      key_exit= types.InlineKeyboardButton(text='Выйти', callback_data='exit')
+      keyboard.add(key_exit) #добавляем кнопку в клавиатуру
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "currency":
+        pass
+    elif call.data == "exit":
+        pass
 
 def main():
     """
