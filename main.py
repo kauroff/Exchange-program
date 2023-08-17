81361cecf4c583997a325fb45be56a1eefdb3190
Ivan Kaurov <crazyanimalcz@gamil.com>
2023-08-17 16:41:45 +0300

    Completed file

--- main.py
+++ main.py
@@ -1,59 +1,65 @@
-from datetime import datetime
-import requests
 import json
 import os
+from datetime import datetime
 
+import requests
 
 API_KEY = os.getenv('EXCHANGE_RATE_API_KEY')
 CURRENCY_RATES_FILE = 'currency_rates.json'
 
 
 def main():
+    """
+        Основная функция программы.
+        Получает от пользователя название валюты — USD или EUR,
+        получает и выводит на экран текущий курс валюты от API.
+        Записывает данные в JSON-файл.
+        """
     while True:
-        currency = input('Введите название валюты (USD или EUR): ')
-        if currency.upper() not in ('USD', 'EUR'):
+        currency = input('Введите название валюты (USD или EUR): ').upper()
+        if currency not in ('USD', 'EUR'):
             print('Некорректный ввод')
             continue
 
         rate = get_currency_rate(currency)
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-        print(f'Курс {currency} к рублю: {rate}')
-        data = {'currency': currency, {rate}: rate, {timestamp}: timestamp}
+        print(f'Курс {currency} к рублю: {rate:.2f}')
+        data = {'currency': currency, "rate": rate, "timestamp": timestamp}
         save_to_json(data)
 
-        choice = input('Выберите действие (продолжить или выйти): ')
-        if choice.lower() == 'продолжить':
+        choice = input('Выберите действие (продолжить или выйти): ').lower()
+        if choice == 'продолжить':
             continue
-        elif choice.lower == 'выйти':
+        elif choice == 'выйти':
             break
         else:
             print('Некорректный ввод')
             continue
 
 
-def get_currency_rate(base: str) -> float:
+def get_currency_rate(currency: str) -> float:
     """Получает курс по API и возвращает его в виде float."""
 
-    url = "https://api.apilayer.com/exchangerates_data/latest"
-
-    response = requests.get(url, headers={"apikey": API_KEY}, params={"base": base})
+    url = f"https://api.apilayer.com/exchangerates_data/latest?base={currency}"
+    response = requests.get(url, headers={"apikey": API_KEY})
     rate = response.json()['rates']['RUB']
     return rate
 
 
 def save_to_json(data: dict) -> None:
     """Сохраняет данные в JSON файл"""
+
     with open(CURRENCY_RATES_FILE, 'a') as file:
         if os.stat(CURRENCY_RATES_FILE).st_size == 0:
             json.dump([data], file)
         else:
-            with open(CURRENCY_RATES_FILE) as file:
-                data_list = json.load(file)
-                data_list.append(data)
-            with open(CURRENCY_RATES_FILE, 'w') as file:
-                json.dump(data_list, file)
+            with open(CURRENCY_RATES_FILE) as json_file:
+                data_list = json.load(json_file)
+            data_list.append(data)
+            with open(CURRENCY_RATES_FILE, 'w') as json_file:
+                json.dump(data_list, json_file)
 
 
-if __name__ == '__main':
+if __name__ == '__main__':
     main()
